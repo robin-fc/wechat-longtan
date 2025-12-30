10b4093a2dbc3849b0f9a1f8c1512de264915a5a
victor <40204871+robin-fc@users.noreply.github.com>
2025-12-30 21:52:07 +0800

    feat: 新增手机授权码换手机号的接口

--- api/auth.ts
+++ api/auth.ts
@@ -3,11 +3,15 @@ import type {
   AppWeixinMiniAppCodeLoginReqVO,
   AppWeixinMiniAppLoginRespVO,
   AppRefreshTokenReqVO,
+  AppGetPhoneNumberReqVO,
+  AppGetPhoneNumberRespVO,
 } from '../model/auth'
+
+const baseUrl = "/app-api/daolongtan/auth"
 
 export function login(data: AppWeixinMiniAppCodeLoginReqVO): Promise<AppWeixinMiniAppLoginRespVO> {
   return request<AppWeixinMiniAppLoginRespVO>({
-    url: '/app-api/daolongtan/auth/weixin-mini-app-code-login',
+    url: `${baseUrl}/weixin-mini-app-code-login`,
     method: 'POST',
     data,
   }).then((res) => res.data)
@@ -17,12 +21,21 @@ export function refreshToken(
   data: AppRefreshTokenReqVO
 ): Promise<AppWeixinMiniAppLoginRespVO> {
   return request<AppWeixinMiniAppLoginRespVO>({
-    url: '/app-api/daolongtan/auth/refresh-token',
+    url: `${baseUrl}/refresh-token`,
     method: 'POST',
     data,
     // 刷新接口不依赖现有 accessToken，直接使用后端允许的占位认证头
     headers: {
       Authorization: 'Bearer test1',
     },
+  }).then((res) => res.data)
+}
+
+// 手机号授权码换取手机号信息
+export function postPhoneNumber(data: AppGetPhoneNumberReqVO): Promise<AppGetPhoneNumberRespVO> {
+  return request<AppGetPhoneNumberRespVO>({
+    url: `${baseUrl}/get-phone-number`,
+    method: 'POST',
+    data,
   }).then((res) => res.data)
 }
--- model/auth.ts
+++ model/auth.ts
@@ -13,3 +13,19 @@ export interface AppWeixinMiniAppLoginRespVO {
 export interface AppRefreshTokenReqVO {
   refreshToken: string
 }
+
+export interface AppGetPhoneNumberReqVO {
+  // 手机号授权码，小程序通过 button 组件的 open-type='getPhoneNumber' 获取,示例值(081abc123)		true	
+  phoneCode: string;
+}
+
+export interface AppGetPhoneNumberRespVO {
+  /*用户绑定的手机号（国外手机号会有区号） */
+  phoneNumber: string;
+
+  /*没有区号的手机号 */
+  purePhoneNumber: string;
+
+  /*区号 */
+  countryCode: string;
+}
